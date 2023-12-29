0bc4e61ab845cbb0f4143528360d06a7e5bea0a4
Bombastik1 <plutenko.ay@gmail.com>
2023-12-29 15:23:32 +0200

    Добавьте файлы проекта.

---
+++ Lab_15(1)_Plutenko/Lab_15(1)_Plutenko.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h> 
+#include <limits.h> 
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "");
+    int n = 2;
+    int i;
+    int* array = (int*)malloc(n * sizeof(int));
+
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < n; i++) {
+        array[i] = rand() % 201 - 100;
+    }
+
+    if (array[0] > array[1]) {
+        printf("Перше число: %d \n", array[0]);
+    }
+    else {
+        printf("Перше число: %d\n", array[0]);
+        printf("Друге число: %d\n", array[1]);
+    }
+
+    return 0;
+}
